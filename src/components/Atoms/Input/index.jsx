d2beb11f1932e8456117c4222495521ebe40ec86
minamism <greenmint2001@icloud.com>
2024-03-11 22:26:00 +0900

    feature: Atoms_Input

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+import FONTFAMILY from "../../../variables/font_family";
+
+export const Input = ({ onEditComplete, defaultValue }) => {
+  const inputRef = useRef(null);
+
+  const handleBlur = () => {
+    onEditComplete(inputRef.current.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onEditComplete(inputRef.current.value);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <StyledWrapper>
+      <StyledInput
+        ref={inputRef}
+        defaultValue={defaultValue}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
+      />
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+
+`;
+const StyledInput = styled.input`
+  ${TEXT.S};
+  color: ${COLOR.LIGHT_GRAY};
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  background-color: ${COLOR.BLACK};
+  border: none;
+  border-radius: 2px;
+  padding: 0 4px;
+  width: 100%;
+  outline: none;
+`;
